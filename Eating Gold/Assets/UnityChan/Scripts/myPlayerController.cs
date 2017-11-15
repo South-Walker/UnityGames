7c0f0c6c8dfb2f479140b3d004d237b93622c548
Nanxing Xiao <506830915@qq.com>
2017-11-15 23:23:14 +0800

    2017-11-15

--- Eating Gold/Assets/UnityChan/Scripts/myPlayerController.cs
+++ Eating Gold/Assets/UnityChan/Scripts/myPlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class myPlayerController : MonoBehaviour {
     private Animator animator;
+    public float watcher;
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -15,8 +16,41 @@ public class myPlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        float speed = animator.GetFloat("Speed");
+        float direction = animator.GetFloat("Direction");
+        if (speed == 0)
+        {
+            animator.SetBool("isTired", animator.GetBool("hasRun") && true);
+        }
+        else
+        {
+            animator.SetBool("hasRun", true);
+            animator.SetBool("isTired", false);
+            this.transform.position = GetNextPosition(speed, direction);
+        }
+    }
+    private void LateUpdate()
+    {
         float direction = Input.GetAxis("Horizontal");
-        //查查看看
-        animator.SetFloat("Direction", direction,0.25f, Time.deltaTime);
-	}
+        float speed = Mathf.Abs(direction) + Input.GetAxis("Vertical");
+        animator.SetFloat("Speed", speed, 0.25f, Time.deltaTime);
+        animator.SetFloat("Direction", direction, 0.25f, Time.deltaTime);
+        if (animator.GetFloat("Speed") <= 0.1f)
+        {
+            animator.SetFloat("Speed", 0);
+        }
+    }
+    
+    public Vector3 GetNextPosition(float speed, float direction)
+    {
+        speed = 0.02f * speed;
+        Vector3 front = this.transform.forward;
+
+        float myDirection = 1 - direction;
+        float rad = (1 / 2.0f * Mathf.PI * myDirection / 2.0f + 1 / 4.0f * Mathf.PI);
+        float nextx = speed * Mathf.Cos(rad);
+        float nextz = speed * Mathf.Sin(rad);
+        Vector3 nextposition = new Vector3(nextx, 0, nextz);
+        return this.transform.position + nextposition;
+    }
 }
--- README.md
+++ README.md
@@ -18,7 +18,7 @@ unity学习笔记
 >```
 >* 要注意旋转这个属性不简单是一个三元数（可以用三元数实例化Quaternion）用的是欧拉旋转法，每个参数分别表示绕x，y，z轴旋转度数。<a href="http://blog.csdn.net/candycat1992/article/details/41254799" target="_blank" title="dachuang">关于旋转的具体说明，有时间再看</a>
 
-#### 2017-11-13
+#### 2017-11-14
 
 >```c#
 >IAnimatorable.SetFloat(string name, float value, float dampTime, float deltaTime);
@@ -27,3 +27,12 @@ unity学习笔记
 >```c#
 >value = current + (target - current) * deltaTime / (dampTime + deltaTime); 
 >```
+
+>```c#
+>Vector3 front = this.transform.forward;
+>```
+>* transform的forward这个属性返回的是当前对象z轴在大坐标上对应向量的单位向量，注意在Unity的注释里，是用颜色表示笛卡尔坐标轴的，blue axis指的就是z轴
+
+#### 2017-11-15
+
+>* 初步解决了基本的运动（前、左、右），但是当物体朝向（this.transform.forward）改变时原有算法失效，考虑完善，不然没法处理转向问题
