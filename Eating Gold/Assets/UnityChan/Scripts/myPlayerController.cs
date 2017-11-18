b76584ffefd0bb22b4dc0d5a5cf2e69774cc9d02
Nanxing Xiao <506830915@qq.com>
2017-11-18 16:19:26 +0800

    2017-11-18

--- Eating Gold/Assets/UnityChan/Scripts/myPlayerController.cs
+++ Eating Gold/Assets/UnityChan/Scripts/myPlayerController.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class myPlayerController : MonoBehaviour {
     private Animator animator;
-    private float Speed
+    public float Speed
     {
         get
         {
             return animator.GetFloat("Speed");
         }
+        set
+        {
+            animator.SetFloat("Speed", value);
+        }
+    }
+    public float SmoothSetSpeed
+    {
         set
         {
             animator.SetFloat("Speed", value, 0.25f, Time.deltaTime);
@@ -26,8 +33,7 @@ public class myPlayerController : MonoBehaviour {
             animator.SetFloat("Direction", value);
         }
     }
-    public bool isbigger;
-    public bool isdifferentside;
+    public float Rotation = 0;
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -47,18 +53,22 @@ public class myPlayerController : MonoBehaviour {
         {
             animator.SetBool("hasRun", true);
             animator.SetBool("isTired", false);
-            Vector3 nextposition = GetNextPosition(Speed, Direction);
-            this.transform.forward = GetForward(nextposition);
-            this.transform.position = nextposition;
+            this.transform.rotation = Quaternion.Euler(0, Rotation, 0);
+            this.transform.position = GetNextPosition(Speed, Direction, Mathf.PI / 4f, Mathf.PI / 4f * 3f);
         }
     }
     private void LateUpdate()
     {
-        Direction = Input.GetAxis("Horizontal");
-        Speed = Mathf.Abs(Direction) + Input.GetAxis("Vertical");
-        if (animator.GetFloat("Speed") <= 0.1f)
+        float newdirection = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(newdirection) >= Mathf.Abs(Direction) || newdirection * Direction < 0) 
         {
-            animator.SetFloat("Speed", 0);
+            Rotation += newdirection;
+        }
+        Direction = newdirection;
+        SmoothSetSpeed = Mathf.Abs(Direction) + 2 * Mathf.Abs(Input.GetAxis("Vertical"));
+        if (Speed <= 0.1f)
+        {
+            Speed = 0;
         }
     }
 
@@ -67,7 +77,6 @@ public class myPlayerController : MonoBehaviour {
         speed = 0.02f * speed;
         float myDirection = 1f - direction;
         float rad = ((endrad - beginrad) * myDirection / 2f + beginrad);
-        
 
         Vector3 nextstep = GetNextStep(speed, rad);
         return this.transform.position + nextstep;
@@ -76,35 +85,10 @@ public class myPlayerController : MonoBehaviour {
     {
         float nextx = _speed * Mathf.Cos(rad);
         float nextz = _speed * Mathf.Sin(rad);
+        Vector3 front = this.transform.forward;
+        Vector3 right = Vector3.Cross(new Vector3(0, 1, 0), front);
         //这个是相对于物体坐标系的
-        Vector3 nextstep = new Vector3(nextx, 0, nextz);
-        Vector3 smallz = this.transform.forward;
-        //y==0
-        Vector3 smallx = new Vector3(smallz.z, 0, smallz.x * -1f);
-
-        nextstep = Vector3.Dot(smallz, nextstep) * smallz + Vector3.Dot(smallx, nextstep) * smallx;
-        
-
-
+        Vector3 nextstep = nextz * front + nextx * right;
         return nextstep;
-    }
-    private Vector3 GetForward(Vector3 nextposition)
-    {
-        Vector3 nowforward = (nextposition - this.transform.position).normalized;
-        return nowforward;
-
-        Vector3 beforeforward = this.transform.forward;
-        Vector3 blueaxis = new Vector3(0, 0, 1);
-        bool isdifferentside = (Vector3.Cross(nowforward, blueaxis) != Vector3.Cross(beforeforward, blueaxis)) ? true : false;
-        bool isbiggerinrad = (Vector3.Dot(nowforward, blueaxis) <= Vector3.Dot(beforeforward, blueaxis)) ? true : false;
-
-        isbigger = isbiggerinrad;
-        this.isdifferentside = isdifferentside;
-
-        if (isdifferentside || isbiggerinrad) 
-        {
-            return nowforward;
-        }
-        return beforeforward;
     }
 }
